01b6a95b24545c01d43b9910c489b3c9f232adde
raaed-ilham <raaed124w@gmail.com>
2025-05-05 16:43:20 +0530

    added prototype session persist using Local Storage

--- src/app/store/useSessionStore.ts
+++ src/app/store/useSessionStore.ts
@@ -1,15 +1,26 @@
+// app/store/useSessionStore.ts
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface SessionState {
-    isAuthenticated: boolean;
     email: string | null;
+    isAuthenticated: boolean;
     login: (email: string) => void;
     logout: () => void;
 }
 
-export const useSessionStore = create<SessionState>((set) => ({
-    isAuthenticated: false,
-    email: null,
-    login: (email: string) => set({ isAuthenticated: true, email }),
-    logout: () => set({ isAuthenticated: false, email: null }),
-}));
+export const useSessionStore = create<SessionState>()(
+    persist(
+        (set) => ({
+            email: null,
+            isAuthenticated: false,
+            login: (email) =>
+                set({ email, isAuthenticated: true }),
+            logout: () => set({ email: null, isAuthenticated: false }),
+        }),
+        {
+            name: "session-storage", // key in localStorage
+            storage: createJSONStorage(() => localStorage),
+        }
+    )
+);
